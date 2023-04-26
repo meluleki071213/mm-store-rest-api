7cf1fa35bc77d8b8f533817e65de098719a4eee2
a233057 <meluleki.madlala@standardbank.co.za>
2023-04-27 00:08:21 +0200

    Initial springboot app setup

---
+++ src/test/java/com/mm/storerestapi/StoreRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mm.storerestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
